e3ace79f6a5e183e13b832a930c0b320befc095a
Nicholas Neuman <neecholaus@gmail.com>
2018-03-05 22:06:29 -0800

    Minor changes

--- paint.py
+++ paint.py
@@ -1,33 +1,7 @@
 from tkinter import *
+import io
 
 class Paint:
-    def __init__(self):
-        self.window = Tk()
-
-        self.canvas = Canvas(self.window, width="500", height="500", bg="white")
-        self.canvas.pack()
-
-        nav = Frame(self.window, relief="sunken")
-        nav.pack(side=BOTTOM, fill=X)
-
-        quitBtn = Button(nav, text="Quit", command=self.window.destroy)
-        quitBtn.pack(side=RIGHT)
-
-        self.colorEntry = Entry(nav, text="Color")
-        self.colorEntry.pack()
-
-        colorBtn = Button(nav, text="Change Color", command=self.changeColor)
-        colorBtn.pack()
-
-        self.penColor = 'black'
-        self.draw = False
-
-        self.canvas.bind('<Button-1>', self.penOn)
-        self.canvas.bind('<ButtonRelease-1>', self.penOff)
-        self.canvas.bind('<Motion>', self.paint)
-
-        self.window.mainloop()
-
     def penOn(self, event):
         self.draw = True
         self.paint
@@ -40,12 +14,45 @@ class Paint:
         x = event.x
         y = event.y
         if(self.draw == True):
-            self.canvas.create_oval(x, y, x+3, y+3, fill=self.penColor, outline=self.penColor)
+            try:
+                self.canvas.create_oval(x, y, x+3, y+3, fill=self.penColor, outline=self.penColor)
+            except:
+                self.penColor = 'black'
+                self.colorEntry.delete(0, END)
+                self.colorEntry.insert(0, 'black')
 
     def changeColor(self):
         color = self.colorEntry.get()
-        self.penColor = color
+        self.penColor = color        
 
+    def __init__(self):
+        self.window = Tk()
+        self.window.title('Python Paint')
+
+        self.canvas = Canvas(self.window, width="500", height="500", bg="white")
+        self.canvas.pack()
+
+        nav = Frame(self.window, relief="sunken")
+        nav.pack(side=BOTTOM, fill=X)
+
+        quitBtn = Button(nav, text="Quit", command=self.window.destroy)
+        quitBtn.pack(side=RIGHT)
+
+        self.colorEntry = Entry(nav)
+        self.colorEntry.pack(side=LEFT)
+        self.colorEntry.insert(0, 'black')
+
+        colorBtn = Button(nav, text="Change Color", command=self.changeColor)
+        colorBtn.pack(side=LEFT)
+
+        self.penColor = 'black'
+        self.draw = False
+
+        self.canvas.bind('<Button-1>', self.penOn)
+        self.canvas.bind('<ButtonRelease-1>', self.penOff)
+        self.canvas.bind('<Motion>', self.paint)
+
+        self.window.mainloop()
 
 
 app = Paint()
